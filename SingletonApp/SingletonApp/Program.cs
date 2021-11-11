e572828e1d290c88f0846e0cd147b7e6d42b49da
Bálint Krsják <kb9719@gmail.com>
2021-11-11 23:13:18 +0100

    singleton & pptx & docker videó

---
+++ SingletonApp/SingletonApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace SingletonApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySingleton mySingleton = MySingleton.GetInstance;
+            mySingleton.DoSomething();
+
+            MyNonSingleton myNonSingleton = new();
+            myNonSingleton.DoSomething();
+        }
+    }
+
+    public class MyNonSingleton
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine("NonSingleton class code has been called");
+        }
+    }
+
+    public class MySingleton
+    {
+        //private static MySingleton instance = new MySingleton();
+        private static MySingleton instance = null;
+        private MySingleton() { }
+
+        public static MySingleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new MySingleton();
+                }
+                return instance;
+            }
+        }
+
+        public void DoSomething()
+        {
+            Console.WriteLine("Singleton class code has been called");
+        }
+    }
+}
